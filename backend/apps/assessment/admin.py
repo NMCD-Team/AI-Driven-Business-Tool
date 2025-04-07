705bd70342ab49ff893a319124b2de9bfa312e00
seandesiree <sean.desiree@gmail.comgit config --global user.email sean.desiree@gmail.comgit versioncdgit config --global user.email sean.desiree@gmail.com>
2025-04-06 20:29:28 -0400

    restructures files, configured backend and implements spacy

---
+++ backend/apps/assessment/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import BusinessAssessment
+
+@admin.register(BusinessAssessment)
+class BusinessAssessmentAdmin(admin.ModelAdmin):
+    list_display = ['company_name', 'email', 'created_at']
+    search_fields = ['company_name', 'email', 'full_name']
+    list_filter = ['created_at', 'industry_niche']
+    readonly_fields = ['created_at', 'updated_at']
